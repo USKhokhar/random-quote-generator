03368fec7f22a7f4b5d6f237b35e1896ae156b59
U.S.Khokhar <63156376+USKhokhar@users.noreply.github.com>
2022-10-04 15:04:22 +0530

    copy-btn-added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const quoteJs = document.querySelector('blockquote');
 const authorJs = document.querySelector('cite');
+const copyBtn = document.querySelector('.copy');
 const reloadBtn = document.querySelector('.reload');
 
 // Calling Function
@@ -19,6 +20,15 @@ getQuote = async () => {
    }
 
 }
+
+// Copying mechanism
+copyBtn.addEventListener('click', () => {
+   navigator.clipboard.writeText(quoteJs.innerText).then(() => {
+      alert('Quote copied to Clipboard')
+   }, (err) => {
+      alert('Failed to copy to clipboard!')
+   });
+})
 
 reloadBtn.addEventListener('click', () => {
    getQuote();
